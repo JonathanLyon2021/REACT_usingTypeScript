18090b87445e24a10dfab301f1f5b5ffcbf0ff05
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-11-14 22:41:44 -0500

    Added some objects

--- App.tsx
+++ App.tsx
@@ -7,3 +7,24 @@ let age: number | string;
 let isStudent: boolean;
 let hobbies: string[];
 let role: [number, string];
+
+//function printName(name: string) {
+//  console.log(name);
+//}
+let printName: (name: string) => never;
+
+let personName: unknown;
+
+interface Person {
+  name: string;
+  age?: number;
+}
+
+interface Guy extends Person {
+  profession: string;
+}
+
+type x = {
+  a: string;
+  b: number;
+}
