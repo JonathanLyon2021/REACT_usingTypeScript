029c97b9ed464d1fa7a12e6978995e483b8edb0e
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-11-14 22:39:24 -0500

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+let name: string = "Jonathan";
+let age: number | string;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
